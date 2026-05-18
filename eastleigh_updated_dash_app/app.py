4a55b4d21a9db276546c9aaa37fafdea249763d3
maritzlindy1-hue <maritz.lindy1@gmail.com>
2026-05-18 10:35:22 +0200

    Add files via upload

--- eastleigh_updated_dash_app/app.py
+++ eastleigh_updated_dash_app/app.py
@@ -399,4 +399,12 @@ app.layout = html.Div(
 
 if __name__ == "__main__":
     print("Dashboard starting... open http://127.0.0.1:8050")
-    app.run(host="127.0.0.1", port=8050, debug=False)
+    import os
+
+port = int(os.environ.get("PORT", 8050))
+
+app.run(
+    host="0.0.0.0",
+    port=port,
+    debug=False
+)
